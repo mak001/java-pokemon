e4a6f14d5e4989d6deddbad0b7ccfe0e37ec18dd
Matthew Koerber <mak001001@hotmail.com>
2012-05-21 13:12:10 -0500

    split some images did a little work with the code

--- src/com/pokemon/room/Viewport.java
+++ src/com/pokemon/room/Viewport.java
@@ -1,6 +1,28 @@
 package com.pokemon.room;
 
+import java.awt.Rectangle;
+
+import com.pokemon.root.GeneralCharacter;
 
 public class Viewport {
+
+	Rectangle rect;
+	GeneralCharacter c;
+
+	public Viewport(GeneralCharacter c) {
+		this.c = c;
+		rect = new Rectangle(c.getCoordinate().getOffsetX() - 100, c
+				.getCoordinate().getOffsetY() - 200, 200, 400);
+	}
+
+	public void setView(GeneralCharacter c) {
+		this.c = c;
+		rect = new Rectangle(c.getCoordinate().getOffsetX() - 100, c
+				.getCoordinate().getOffsetY() - 200, 200, 400);
+	}
+
+	public Rectangle getBounds() {
+		return rect;
+	}
 
 }
--- src/com/pokemon/room/characters/NPC.java
+++ src/com/pokemon/room/characters/NPC.java
@@ -1,6 +1,8 @@
 package com.pokemon.room.characters;
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.util.ArrayList;
 
 import com.pokemon.GameBase;
@@ -17,17 +19,24 @@ public class NPC extends GeneralCharacter {
 
 	private ArrayList<Coordinate> path = new ArrayList<Coordinate>();
 	private int current_coord = -1;
+	private Type type;
+
+	public enum Type {
+		PROF_OAK, POLICE, SAILOR, CAPTAIN, ROCKET_MALE, ROCKET_FEMALE,
+		SWIMMER_MALE, SWIMMER_FEMALE, BIKER, BUG_CATCHER, CAMPER_MALE,
+		CAMPER_FEMALE, HIKER, SCIENTIST, CHEF, TRAINER_MALE, TRAINER_FEMALE
+	}
 
 	public NPC(String[] text, ArrayList<Coordinate> path) {
-		this("", null, text, path);
+		this("", null, null, text, path);
 	}
 
-	public NPC(String name, String[] text, ArrayList<Coordinate> path) {
-		this(name, null, text, path);
+	public NPC(Type type, String[] text, ArrayList<Coordinate> path) {
+		this("", type, null, text, path);
 	}
 
-	public NPC(String name, ArrayList<Pokemon> pokemon, String[] text,
-			ArrayList<Coordinate> path) {
+	public NPC(String name, Type type, ArrayList<Pokemon> pokemon,
+			String[] text, ArrayList<Coordinate> path) {
 		this.name = name;
 		this.pokemon = pokemon;
 		this.text = text;
@@ -35,6 +44,28 @@ public class NPC extends GeneralCharacter {
 		if (this.path != null) {
 			current_coord = 0;
 		}
+		setImage();
+	}
+
+	private void setImage() {
+		switch (type) {
+		case POLICE:
+			image = image("police", false);
+			break;
+		case SAILOR:
+			image = image("sailor", false);
+			break;
+
+		}
+	}
+
+	public Image image(String name, boolean battle) {
+		if (battle)
+			return Toolkit.getDefaultToolkit().getImage(
+					NPC.class.getResource("/images/battle/trainers/" + name
+							+ ".png"));
+		return Toolkit.getDefaultToolkit().getImage(
+				NPC.class.getResource("/images/room/" + name + ".png"));
 	}
 
 	public int getSpeechLength() {
@@ -61,9 +92,15 @@ public class NPC extends GeneralCharacter {
 	public void draw(Graphics g) {
 		if (current_coord != -1) { // -1 being that it doesn't have a path
 			GameBase.getRoom().setTile(path.get(current_coord + 1), this);
+		} else {
+
 		}
 		// TODO Auto-generated method stub
 
+	}
+
+	public int getDrawY() {
+		return getCoordinate().getY() + 4;
 	}
 
 }
